3a7f7f54c8caf13546b0ec2e3f67d97675d56aea
scheng20 <chengsheena25@gmail.com>
2021-03-01 20:17:55 -0700

    Finished Basic Game Logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
+import GomokuGame from './Game/Gomoku.js';
+
+let game = new GomokuGame(19);
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Gomoku Online </h1>
-      <p> An online port of the classic game: Gomoku </p>
-    </div>
-  );
+
+	// TEST SUITE
+	/*
+	game.play(0,0);
+	game.play(1,0);
+	game.play(0,1);
+	game.play(2,0);
+	game.play(0,2);
+	game.play(3,0);
+	game.play(0,3);
+	game.play(4,0);
+	game.play(0,4);
+	*/
+
+	return (
+		<div className="App">
+			<h1> Gomoku Online </h1>
+			<p> An online port of the classic game: Gomoku </p>
+		</div>
+	);
 }
 
 export default App;
--- src/Game/Gomoku.js
+++ src/Game/Gomoku.js
@@ -2,6 +2,8 @@
  * Gomoku.js - Game logic for the board game Gomoku
  */
 
+const Gomoku = require('./Main');
+
 let STONE_EMPTY = 0;
 let STONE_BLACK = 1;
 let STONE_WHITE = 2;
@@ -12,6 +14,7 @@ export default class GomokuGame {
 		this.currentColor = STONE_BLACK;
 		this.size = size;
 		this.board = this.createBoard(size);
+		this.game = new Gomoku(5, size, size);
 	}
 
 	// Returns a matrix representing an empty game board
@@ -29,25 +32,34 @@ export default class GomokuGame {
 
 	// Switches the turn
 	switchPlayer() {
-		if (this.currentColor == STONE_BLACK) {
+
+		if (this.currentColor === STONE_BLACK) {
 			this.currentColor = STONE_WHITE;
 		} else {
 			this.currentColor = STONE_BLACK;
 		}
+
+		console.log("SWITCH! The currentColor is now: " + this.currentColor);
 	}
 
 	// Ends the game and displays a winner message (if any)
 	endGame(winner) {
 
-		if (winner !== "none") {
-			alert({winner} + "WON!");
+		if (winner === 1) {
+			alert("BLACK WON!");
+		} else if (winner === 2) {
+			alert("WHITE WON!");
 		} else {
-			alert ("DRAW GAME");
+			alert("DRAW GAME");
 		}
 
 	}
 
 	// Returns true if the game is in a draw, false otherwise
+	// TODO - Make this more efficient, perhaps keep track of 
+	// the total number of stones played and if that equals
+	// the total number of squares then it's a draw
+	/*
 	detectDraw() {
 
 		for (let i = 0; i < this.size; i++) {
@@ -59,11 +71,33 @@ export default class GomokuGame {
 		}
 
 		return true; 
-	}
+	}*/
 
 	// Attempts to play a stone at position i, j
 	play(i, j) {
-		// TODO
+
+		try {
+			
+			console.log(this.currentColor + " is playing at " + i + ", " + j);
+			
+			let result = this.game.setChessOf(this.currentColor, i, j);
+			
+			this.board[i][j] = this.currentColor;
+			this.switchPlayer();
+
+			if(result !== null && typeof result !== 'undefined') {
+				this.endGame(result);
+			}
+
+		} catch (error) {
+			
+			if (error instanceof ReferenceError) {
+				console.log("That space has been taken!");
+			} else {
+				console.log("An error occured:");
+				console.error(error)
+			}
+		}
 	}
 
 }

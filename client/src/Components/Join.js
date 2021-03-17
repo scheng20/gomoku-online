aaa7c16c8edaa7d8df60419a1005407d4dadf711
Sheena Cheng <chengsheena25@gmail.com>
2021-03-16 20:48:04 -0600

    Added socket.io to application

--- client/src/Components/Join.js
+++ client/src/Components/Join.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import io from 'socket.io-client';
+
+let socket;
 
 export default function Join() {
+
+	const ENDPOINT = 'localhost:5000';
+
+	useEffect(() => {
+
+		socket = io(ENDPOINT, { transports : ['websocket'] });
+		
+		socket.emit('join', () => {});
+
+		return () => {
+			socket.emit('disconnect');
+			socket.off();
+		}
+
+	}, [ENDPOINT]);
 
 	return (
 		<h1> Join </h1>
--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+	console.log('We have a new connection!');
+
+	socket.on('join', () => {
+		console.log('Visited join page');
+	});
+	
+	socket.on('disconnect', () => {
+		console.log('User has left.');
+	});
+});
+
 app.use(router);
 
 server.listen(PORT, () => {
